b9386c9b7681d9612df047cc1eb85b41468caa86
Victor <vramirez.info@gmail.com>
2019-09-11 19:32:23 -0500

    Modificacion de API React

--- server.js
+++ server.js
@@ -1,64 +1,106 @@
-const express = require('express')
+let express = require('express');
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+let pg  = require ('pg');
+let md5 = require('md5');
+const PORT = 3000;
 
-// use process.env variables to keep private variables,
-require('dotenv').config()
+let pool = new pg.Pool({
 
-// Express Middleware
-const helmet = require('helmet') // creates headers that protect from attacks (security)
-const bodyParser = require('body-parser') // turns response into usable format
-const cors = require('cors')  // allows/disallows cross-site communication
-const morgan = require('morgan') // logs requests
+    user:'postgres',
+    database: 'School',
+    password:'postgres',
+    host: 'localhost',
+    port:5432,
+    max:10
 
-// db Connection w/ Heroku
-// const db = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: true,
-//   }
-// });
-
-// db Connection w/ localhost
-var db = require('knex')({
-    client: 'pg',
-    connection: {
-        host : '127.0.0.1',
-        user : '',
-        password : '',
-        database : 'crud-practice-1'
-    }
 });
 
-// Controllers - aka, the db queries
-const main = require('./controllers/main')
 
-// App
-const app = express()
 
-// App Middleware
-const whitelist = ['http://localhost:3001']
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+/*pool.connect((err,db,done) => {
+
+    if(err){
+        return console.log(err);
     }
-}
-app.use(helmet())
-app.use(cors(corsOptions))
-app.use(bodyParser.json())
-app.use(morgan('combined')) // use 'tiny' or 'combined'
+    else{
 
-// App Routes - Auth
-app.get('/', (req, res) => res.send('hello world'))
-app.get('/crud', (req, res) => main.getTableData(req, res, db))
-app.post('/crud', (req, res) => main.postTableData(req, res, db))
-app.put('/crud', (req, res) => main.putTableData(req, res, db))
-app.delete('/crud', (req, res) => main.deleteTableData(req, res, db))
+        db.query('SELECT username, email, created_at FROM public.users', (err,table) => {
 
-// App Server Connection
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT || 3000}`)
-})
+            done();
+
+            if(err){
+                return console.log(err);
+            }
+            else{
+              console.log(table.rows);
+            }
+        })
+
+    }
+})*/
+
+
+
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(morgan('dev'));
+
+app.use(function(request,response,next){
+
+    response.header("Access-Control-Allow-Origin","*");
+    response.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+app.post('/api/users/new', function(request,response){
+
+    var username = request.body.username;
+    var password = request.body.password;
+    var email = request.body.email;
+
+    var dateTimeToday = new Date();
+    var today =new Date(dateTimeToday.getFullYear(),
+        (dateTimeToday.getMonth() + 1) /*Jan = 0! */,
+        dateTimeToday.getDate(),
+        dateTimeToday.getHours(),
+        dateTimeToday.getMinutes(),
+        dateTimeToday.getSeconds())
+
+
+    pool.connect((err, db, done) => {
+
+        if (err) {
+            return response.status(400).send(err);
+        } else {
+
+
+            db.query('INSERT INTO users (username,password,email,created_at) VALUES ($1,$2,$3,$4)', [username, md5(password), email ,today], (err, table) => {
+
+                done();
+
+                if (err) {
+                    return console.log(err);
+                } else {
+                    console.log("SUCCESSFULLY");
+                    response.status(200).send({message:'El usuario ha sido creado con éxito'});
+                    db.end();
+                    console.log(table.rows);
+                }
+            })
+
+        }
+    })
+
+
+
+
+
+});
+
+app.listen(PORT,() => console.log('Listening on Port' + PORT));
