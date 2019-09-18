e0306205bad27332aa9d28df561379d569a63d96
Victor <vramirez.info@gmail.com>
2019-09-17 19:02:42 -0500

    Arreglar el método post

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let morgan = require('morgan');
 let pg  = require ('pg');
-let md5 = require('md5');
 const PORT = 3000;
 
 let pool = new pg.Pool({
@@ -15,33 +14,6 @@ let pool = new pg.Pool({
     max:10
 
 });
-
-
-
-/*pool.connect((err,db,done) => {
-
-    if(err){
-        return console.log(err);
-    }
-    else{
-
-        db.query('SELECT username, email, created_at FROM public.users', (err,table) => {
-
-            done();
-
-            if(err){
-                return console.log(err);
-            }
-            else{
-              console.log(table.rows);
-            }
-        })
-
-    }
-})*/
-
-
-
 
 let app = express();
 
@@ -57,6 +29,8 @@ app.use(function(request,response,next){
     next();
 });
 
+//Direcciones de ENDPOINTS DEL API PARA POSEIDON
+
 
 app.post('/api/users/new', function(request,response){
 
@@ -66,7 +40,7 @@ app.post('/api/users/new', function(request,response){
 
     var dateTimeToday = new Date();
     var today =new Date(dateTimeToday.getFullYear(),
-        (dateTimeToday.getMonth() + 1) /*Jan = 0! */,
+        (dateTimeToday.getMonth() + 1),
         dateTimeToday.getDate(),
         dateTimeToday.getHours(),
         dateTimeToday.getMinutes(),
@@ -80,7 +54,7 @@ app.post('/api/users/new', function(request,response){
         } else {
 
 
-            db.query('INSERT INTO users (username,password,email,created_at) VALUES ($1,$2,$3,$4)', [username, md5(password), email ,today], (err, table) => {
+            db.query('INSERT INTO users (username,password,email,created_at) VALUES ($1,$2,$3,$4)', [username,password, email ,today], (err, table) => {
 
                 done();
 
@@ -90,7 +64,7 @@ app.post('/api/users/new', function(request,response){
                     console.log("SUCCESSFULLY");
                     response.status(200).send({message:'El usuario ha sido creado con éxito'});
                     db.end();
-                    console.log(table.rows);
+
                 }
             })
 
